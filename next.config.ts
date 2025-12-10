a3091445d96d8447d1d608ffda96010803c00023
Adepoju Ololade Joshua Ilerioluwa <adepojuololade2020@gmail.com>
2025-12-10 14:27:46 +0100

    eslint errors

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+interface CustomNextConfig extends NextConfig {
+  eslint?: {
+    ignoreDuringBuilds: boolean;
+  };
+}
+
+const nextConfig: CustomNextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // webpack5: false,
-
-  // serverExternalPackages: ["mongoose"],
-
-  // experimental: {
-  //   // appDir: true,
-  //   esmExternals: "loose", // <-- add this
-  //   serverComponentsExternalPackages: ["mongoose"] // <-- and this
-  // },
-
-  // and the following to enable top-level await support for Webpack
-  // webpack: (config) => {
-  //   config.experiments = {
-  //     ...config.experiments,
-  //     topLevelAwait: true
-  //   };
-  //   return config;
-  // },
 };
 
 export default nextConfig;
